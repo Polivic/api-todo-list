a48bd8250d7cb4a154c93f0586eb91fbced6f18b
Polivic <polianavitoriabh@gmail.com>
2026-01-28 23:16:32 -0300

    Corrige configuração do banco SQLite para Render

--- src/config/database.js
+++ src/config/database.js
@@ -1,8 +1,8 @@
 const { Sequelize } = require('sequelize');
-require('dotenv').config();
 
-const sequelize = new Sequelize(process.env.DATABASE_URL, {
+const sequelize = new Sequelize({
   dialect: 'sqlite',
+  storage: './database.sqlite',
   logging: false
 });
 
